30f0107c8eb5fee7eef7c058d868149c583e0055
Isasbeula <133120325+Isasbeula@users.noreply.github.com>
2024-05-24 09:24:01 -0300

    Update base-conhecimento.pl

--- base-conhecimento.pl
+++ base-conhecimento.pl
@@ -2,3 +2,20 @@
 linguagem(python, desenvolvimento_web).
 linguagem(java, desenvolvimento_web).
 linguagem(javascript, desenvolvimento_web).
+linguagem(c, desenvolvimento_sistemas).
+linguagem(cpp, desenvolvimento_sistemas).
+linguagem(prolog, inteligencia_artificial).
+linguagem(lisp, inteligencia_artificial).
+
+% Regras de recomendação
+recomendacao(Tipo, Linguagem) :-
+    linguagem(Linguagem, Tipo).
+
+recomendacao_por_experiencia(sim, Linguagem) :-
+    Linguagem = python;
+    Linguagem = javascript.
+
+recomendacao_por_experiencia(não, Linguagem) :-
+    Linguagem = java;
+    Linguagem = cpp;
+    Linguagem = prolog.
