543feeeffd02897ec741e47896e1defae036bc0d
Isasbeula <133120325+Isasbeula@users.noreply.github.com>
2024-05-24 09:25:44 -0300

    Update servidor.pl

--- servidor.pl
+++ servidor.pl
@@ -2,3 +2,8 @@
 :- use_module(library(http/http_dispatch)).
 :- use_module(library(http/http_json)).
 :- use_module(library(http/http_parameters)).
+:- [base_de_conhecimento].  % Inclui o arquivo de base de conhecimento
+
+% Iniciar o servidor HTTP
+:- http_handler(root(recomendacao), handle_recomendacao, []).
+
